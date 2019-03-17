1efa5585bbee43f4796115aecd3a9de00a5f1a6d
Vitaly-V <gtopbox@gmail.com>
2019-03-17 17:14:34 +0200

    Add logic

--- bin/main.dart
+++ bin/main.dart
@@ -1,5 +1,7 @@
 import 'dart:io';
 import 'package:prompter_vvn/prompter_vvn.dart';
+import 'package:converter/src/converter.dart';
+
 
 void main() {
   final prompter = new Prompter();
@@ -10,7 +12,14 @@ void main() {
   }
 
   final format = prompter.askMultiple('Select format:', buildFormatOptions());
-  prompter.askMultiple('Select an image to convert: ', buildFileOptions());
+  final selectedFile = prompter.askMultiple('Select an image to convert: ', buildFileOptions());
+  final newPath = convertImage(selectedFile, format);
+
+  final shouldOpen = prompter.askBinary('Open the image?');
+
+  if(shouldOpen) {
+    Process.run('open', [newPath]);
+  }
 }
 
 List<Option> buildFormatOptions() {
@@ -21,5 +30,11 @@ List<Option> buildFormatOptions() {
 }
 
 List<Option> buildFileOptions() {
-
+  return Directory.current.listSync().where((entity) {
+    return FileSystemEntity.isFileSync(entity.path) &&
+        entity.path.contains(new RegExp(r'\.(png|jpg|jpeg)$'));
+  }).map((entity) {
+    final fileName = entity.path.split(Platform.pathSeparator).last;
+    return new Option(fileName, entity);
+  }).toList();
 }
